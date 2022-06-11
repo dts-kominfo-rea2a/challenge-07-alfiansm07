c358be7861420a714d99f4bb03d88fdfcdd49780
alfiansm07 <alfiansm07@gmail.com>
2022-06-11 09:37:02 +0700

    solved

--- promise.js
+++ promise.js
@@ -1,7 +1,17 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    const arrIXX = await promiseTheaterIXX();
+    const arrCGV = await promiseTheaterVGC();
+    const arrConcat = arrIXX.concat(arrCGV);
+    const dataHasil = arrConcat.filter((e) => e.hasil === emosi).length;
+    return dataHasil;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
   promiseOutput,
